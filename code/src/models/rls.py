fc768bfae0d4b598b22875fa38a05cfb2d1b1163
Harish Rajagopal <harish.rajagopals@gmail.com>
2022-05-23 19:35:35 +0200

    Renamed class attributes and methods for clarity

--- code/src/models/rls.py
+++ code/src/models/rls.py
@@ -16,10 +16,10 @@ class RLS(LightningModule):
     Adapted from: http://arxiv.org/abs/2002.09621
     """
 
-    ROWS: Final = 1000
-    COLS: Final = 500
-    LAMBDA: Final = 3.0
-    EPS_SCALE: Final = 0.01
+    NUM_EXAMPLES: Final = 1000
+    NUM_FEATURES: Final = 500
+    CONSTR_WT: Final = 3.0
+    NOISE_STDDEV: Final = 0.01
 
     def __init__(self, config: Config):
         """Initialize and store everything needed for training.
@@ -30,14 +30,14 @@ class RLS(LightningModule):
         super().__init__()
         self.config = config
 
-        self.x = Parameter(torch.zeros(self.COLS, 1))
-        self.y = Parameter(torch.zeros(self.ROWS, 1))
+        self.x = Parameter(torch.zeros(self.NUM_FEATURES, 1))
+        self.y = Parameter(torch.zeros(self.NUM_EXAMPLES, 1))
 
-        self.A = torch.randn(self.ROWS, self.COLS)
-        self.M = torch.eye(self.ROWS)
+        self.A = torch.randn(self.NUM_EXAMPLES, self.NUM_FEATURES)
+        self.M = torch.eye(self.NUM_EXAMPLES)
 
-        x_star = torch.randn(self.COLS, 1)
-        epsilon = torch.randn(self.ROWS, 1) * self.EPS_SCALE
+        x_star = torch.randn(self.NUM_FEATURES, 1)
+        epsilon = torch.randn(self.NUM_EXAMPLES, 1) * self.NOISE_STDDEV
         self.y_0 = self.A @ x_star + epsilon
 
     def configure_optimizers(self) -> Tuple[Dict[str, Any], Dict[str, Any]]:
@@ -67,19 +67,19 @@ class RLS(LightningModule):
         term_2 = self.y - self.y_0.to(self.y.device)
         loss = (
             term_1.T @ self.M.to(term_1.device) @ term_1
-            - self.LAMBDA * term_2.T @ self.M.to(term_2.device) @ term_2
+            - self.CONST_WT * term_2.T @ self.M.to(term_2.device) @ term_2
         )
 
         if batch_idx % self.trainer.log_every_n_steps == 0:
             with torch.no_grad():
-                self.log_rls_metrics(batch_idx, loss)
+                self._log_rls_metrics(batch_idx, loss)
 
         if optimizer_idx == 0:  # y update
             return -loss
         else:  # x update
             return loss
 
-    def log_rls_metrics(self, global_step: int, loss: torch.Tensor) -> None:
+    def _log_rls_metrics(self, global_step: int, loss: torch.Tensor) -> None:
         """Log all metrics."""
         if self.x.grad is not None:
             gx = self.x.grad.detach().reshape(-1)
